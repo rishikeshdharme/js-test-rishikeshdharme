758fec03e1d3f90441e465ee22cc4c47c7487c1e
rishikeshdharme <145563510+rishikeshdharme@users.noreply.github.com>
2024-01-05 00:19:21 -0800

    Update program1.js

--- program1.js
+++ program1.js
@@ -3,7 +3,62 @@
  * @return {boolean}
  */
 var isValid = function(s) {
-    
+    const stack =[];
+
+   for( i=0; i<s.length;  i++){
+   
+    let x = s[i];
+ 
+        if (x == '(' || x == '[' || x == '{')
+        {
+             
+          
+            stack.push(x);
+            continue;
+        }
+ 
+        
+        if (stack.length == 0)
+            return false;
+             
+        let check;
+        switch (x){
+        case ')':
+            check = stack.pop();
+            if (check == '{' || check == '[')
+                return false;
+            break;
+ 
+        case '}':
+            check = stack.pop();
+            if (check == '(' || check == '[')
+                return false;
+            break;
+ 
+        case ']':
+            check = stack.pop();
+            if (check == '(' || check == '{')
+                return false;
+            break;
+        }
+    }
+ 
+   
+    return (stack.length == 0);
+}
+ 
+
+let s = "([{}])";
+ 
+
+if (areBracketsBalanced(s))
+    document.write("Balanced ");
+else{
+    document.write("Not Balanced ");
+ 
+
+ 
+
 };
 
 module.exports = { isValid };
