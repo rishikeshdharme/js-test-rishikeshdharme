fb31995cbc8fd765fdedb8d3ebeb51f0c2b3e2f7
rishikeshdharme <145563510+rishikeshdharme@users.noreply.github.com>
2024-01-05 00:43:18 -0800

    Update program2.js

--- program2.js
+++ program2.js
@@ -3,7 +3,39 @@
  * @return {number}
  */
 var romanToInt = function(s) {
-    
+   ' I': 1,
+
+'V': 5.
+
+'X': 10,
+
+'L': 50.
+
+'C': 100
+
+'D': 500,
+
+'M': 1000
+
+);
+
+let result = 0;
+
+for (let i = 0; i < s.length; i++) {
+
+const currentVal = romanValues[s[1]];
+
+const nextVal = romanValues[s[i + 1]];
+
+if (nextVal > currentVal) (
+
+result += (nextVal currentVal);
+
+i++; // Skip the next character as it is already co
+
+) else (
+
+result += currentVal;
 };
 
 
